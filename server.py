d387e785d0831920a00ecd60fd3562355a6f9582
Maor Haim <maorh133@gmail.com>
2020-09-14 12:49:47 +0300

    Add same function for flask

--- server.py
+++ server.py
@@ -1,9 +1,46 @@
 from flask import Flask
+from flask import request
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+companies = []
+employees = []
+
+
+@app.route('/<company_name>', methods=["POST"])
+def set_company(company_name):
+    company = request.get_json()
+    print('Received new company:{}, {}'.format(company_name, company))
+
+    if not all(keys in company for keys in ("rooms", "shifts")):
+        print("[Company] don't have all keys")
+        return {"success": False}
+
+    companies.append(company)
+
+    return {"success": True}
+
+
+@app.route('/<company_name>', methods=["POST"])
+def add_employee(company_name):
+    employee = request.get_json()
+    print('Received new employee:{}, {}'.format(company_name, employee))
+
+    if not all(keys in employee for keys in ("home_location", "car_capacity", "dependencies", "shifts_availability")):
+        print("[employee] don't have all keys")
+        return {"success": False}
+
+    employee["company_name"] = company_name
+    companies.append(employee)
+
+    return {"success": True}
+
+
+@app.route('/<company_name>', methods=["POST"])
+def get_company_data(company_name):
+    rooms = companies[company_name]["rooms"]
+    shifts = companies[company_name]["shifts"]
+    return {"rooms": rooms, "shifts": shifts}
 
 
 if __name__ == '__main__':
